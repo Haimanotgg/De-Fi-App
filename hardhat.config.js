fe0f8d4aa2ff2210728e26556134e6886eb0185d
Haimanotgg <Haimanotgbb@gmail.com>
2023-04-24 00:21:47 +0300

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,17 @@
-require("@nomicfoundation/hardhat-waffle");
-require("@nomicfoundation/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
 require("dotenv").config();
 
-const GOERLI_URL = process.env.GOERLI_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.18",
   networks:  {
-    goerli : {
-      url: GOERLI_URL,
-      account: [PRIVATE_KEY]
+    sepolia : {
+      url: SEPOLIA_URL,
+      accounts: [PRIVATE_KEY]
     }
       
   }
